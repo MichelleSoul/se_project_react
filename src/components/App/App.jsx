a366b20fee30b53603211658292aade8c2a1773a
Michelle Souliotis <michelle.souliotis@gmail.com>
2025-06-28 08:18:14 +0530

    Added Cards with Rendering and Filtering

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,13 +1,15 @@
 import './App.css'
 import Header from "../Header/Header"
 import Main from "../Main/Main"
+import { useState } from 'react'
 
 const App = () => {
+	const [ weatherData, setWeatherData ] = useState({ type: "" });
 	return (
 		<div className='page'>
 			<div className='page__content'>
 				<Header />
-				<Main />
+				<Main weatherData={ weatherData }/>
 			</div>
 		</div>
 	)
