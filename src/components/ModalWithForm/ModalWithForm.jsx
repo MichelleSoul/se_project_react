8e2394e6dd7b13aca726654dcc29e9365a0c060d
Michelle Souliotis <michelle.souliotis@gmail.com>
2025-07-02 08:07:59 +0530

    Created ModalWithForm and Add Garments Modal

---
+++ src/components/ModalWithForm/ModalWithForm.jsx
@@ -0,0 +1,37 @@
+import "./ModalWithForm.css";
+import closeIcon from "../../assets/closeModal.svg";
+
+const ModalWithForm = ({
+    children,
+    buttonText,
+    title,
+    activeModal,
+    handleCloseClick,
+}) => {
+    return (
+        <div
+            className={`modal ${
+                activeModal === "add-garment" && "modal_opened"
+            }`}
+        >
+            <div className="modal__content">
+                <h2 className="modal__title">{title}</h2>
+                <button
+                    onClick={handleCloseClick}
+                    type="button"
+                    className="modal__close"
+                >
+                    <img className="header__close_icon" src={closeIcon} />
+                </button>
+                <form className="modal__form">
+                    {children}
+                    <button type="submit" className="modal__submit">
+                        {buttonText}
+                    </button>
+                </form>
+            </div>
+        </div>
+    );
+};
+
+export default ModalWithForm;
