b446166271bfa921290cb8a8dfc8f238a23a7f77
Michelle Souliotis <michelle.souliotis@gmail.com>
2025-08-06 16:22:48 +0530

    Fixed a typo in ClothesSection directory path

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -1,6 +1,6 @@
 import "./Profile.css";
 import SideBar from "../SideBar/SideBar";
-import ClothesSection from "../ClohtesSection/ClothesSection";
+import ClothesSection from "../ClothesSection/ClothesSection";
 
 const Profile = ({ handleAddClick, handleCardClick, clothingItems }) => {
 	return (
